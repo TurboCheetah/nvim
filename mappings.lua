817eda9aa042227c38f4a0387a64b5c209c48a86
Turbo <dev@turbo.ooo>
2023-10-05 10:14:15 -0500

    chore: add mappings

--- mappings.lua
+++ mappings.lua
@@ -7,9 +7,12 @@ M.general = {
     ["<leader>gg"] = { ":LazyGit<CR>", "open LazyGit", opts = { nowait = true } },
     ["<leader>pm"] = { ":Mason<CR>", "open Mason", opts = { nowait = true } },
     ["|"] = { ":vsplit<CR>", "open a vertical split", opts = { nowait = true } },
+    ["<leader>mpo"] = { ":PeekOpen<CR>", "preview current markdown file", opts = { nowait = true } },
+    ["<leader>mpc"] = { ":PeekClose<CR>", "close preview of current markdown file", opts = { nowait = true } },
   },
   v = {
     [">"] = { ">gv", "indent" },
+    ["<leader>gec"] = { ":Gen Enhance_Code<CR>", "Use gen.nvim and an LLM to enhance code", opts = { nowait = true } },
   },
 }
 
